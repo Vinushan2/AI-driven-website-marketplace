2bef457a3ec66865de34c9aa3ad9777fc39c2803
Vinushan <164358569+Vinushan2@users.noreply.github.com>
2025-08-21 10:14:31 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+// import tailwind-related plugins
+
+export default defineConfig({
+  base: 'https://easydigi.shop/',
+  assetsInclude: ['**/*.webp'],
+  plugins: [
+    // Remove any Tailwind-related plugins
+    // tailwind-related plugins can be removed
+  ],
+});
